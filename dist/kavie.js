8a1706cb64f4e0d2897e7548235e8171cdb9f02c
Matthew Nitschke <mathuc33@gmail.com>
2016-05-04 22:22:11 -0600

    Generated dist/ files. Updated package version

--- dist/kavie.js
+++ dist/kavie.js
@@ -4,120 +4,69 @@
     License: MIT (http://www.opensource.org/licenses/mit-license.php)
 */
 
-ko.extenders.kavie = function (target, rules) {
-    target.hasError = ko.observable(); 
+;(function(ns){
 
-    if (rules.addToArray){
-      Kavie.add(target);
-      delete rules.addToArray;
+  ns.observables = [];
+  ns.sections = [];
+
+  ns.add = function(obs){
+    if (isKavieObservable(obs)){
+      observables = this.observables.concat(obs);
     }
-
-    if (rules.section){
-      if (!Kavie.sections[rules.section]){
-        Kavie.sections[rules.section] = [];
-      }
-
-      Kavie.sections[rules.section].push(target);
-
-      delete rules.section;
-    }
-
-    target.rules = rules;
-
-    function validate(newValue) {
-        var rules = target.rules;
-
-        for (key in rules) {
-            for (funcKey in Kavie.validatorFunctions) {
-                if (key == funcKey) {
-                    var isValid = Kavie.validatorFunctions[funcKey](rules[key], newValue);
-                    if (!isValid) {
-                        target.hasError(true);
-                        return;
-                    }
-                }
-            }
-        }
-
-        target.hasError(false);
-    }
-
-
-    target.startValidation = function () {
-        target.subscription = target.subscribe(validate); 
-        validate(target());
-    }
-
-    target.stopValidation = function () {
-        target.subscription.dispose();
-        target.hasError(false);
-    }
-
-    return target;
-};
-
-
-var Kavie = function(){} 
-Kavie.observables = [];
-Kavie.sections = [];
-
-Kavie.isKavieObservable = function(observable){
-  if (observable.hasOwnProperty("hasError")){ 
-    return true;
-  } else {
-    return false;
   }
-}
 
-Kavie.add = function(obs){
-  if (this.isKavieObservable(obs)){
-    this.observables = this.observables.concat(obs);
-  }
-}
+  ns.isValid = function(vm){
+    var isValid = true;
 
-Kavie.compileObservables = function(vm){
-  var kavieObservables = [];
+    var kavieObservables = compileObservables(vm);
 
-  if (this.observables.length > 0){
-    kavieObservables = kavieObservables.concat(this.observables);
-  }
-  if (vm){
-    var keys = Object.keys(vm);
-    for(var i = 0; i < keys.length; i ++){
-      if (this.isKavieObservable(vm[keys[i]])){
-        kavieObservables.push(vm[keys[i]]);
+    for(var i = 0; i < kavieObservables.length; i ++){
+      kavieObservables[i].startValidation();
+
+      if (isValid) {
+          if (kavieObservables[i].hasError()) {
+              isValid = false;
+          }
       }
     }
+    return isValid;
   }
-  return kavieObservables;
-}
 
-Kavie.isValid = function(vm){
-  var isValid = true;
+  ns.deactivate = function(vm){
+    var kavieObservables = compileObservables(vm);
 
-  var kavieObservables = Kavie.compileObservables(vm);
-
-  for(var i = 0; i < kavieObservables.length; i ++){
-    kavieObservables[i].startValidation();
-
-    if (isValid) {
-        if (kavieObservables[i].hasError()) {
-            isValid = false;
-        }
+    for(var i = 0; i < kavieObservables.length; i ++){
+      kavieObservables[i].stopValidation();
     }
   }
-  return isValid;
-}
 
-Kavie.deactivate = function(vm){
-  var kavieObservables = Kavie.compileObservables(vm);
-
-  for(var i = 0; i < kavieObservables.length; i ++){
-    kavieObservables[i].stopValidation();
+  var isKavieObservable = function(observable){
+    if (observable.hasOwnProperty("hasError")){ 
+      return true;
+    } else {
+      return false;
+    }
   }
-}
 
-Kavie.validatorFunctions = {
+  var compileObservables = function(vm){
+    var kavieObservables = [];
+
+    if (ns.observables.length > 0){
+      kavieObservables = kavieObservables.concat(this.observables);
+    }
+
+    if (vm){
+      var keys = Object.keys(vm);
+      for(var i = 0; i < keys.length; i ++){
+        if (isKavieObservable(vm[keys[i]])){
+          kavieObservables.push(vm[keys[i]]);
+        }
+      }
+    }
+    return kavieObservables;
+  }
+
+  ns.validatorFunctions = {
     required: function (propVal, eleVal) {
         if (propVal) {
             return (eleVal ? true : false);
@@ -184,4 +133,59 @@ Kavie.validatorFunctions = {
         return false;
       }
     }
-}
+  }
+
+}(this.Kavie = this.Kavie || {}));
+
+
+ko.extenders.kavie = function (target, rules) {
+    target.hasError = ko.observable(); 
+
+    if (rules.addToArray){
+      Kavie.add(target);
+      delete rules.addToArray;
+    }
+
+    if (rules.section){
+      if (!Kavie.sections[rules.section]){
+        Kavie.sections[rules.section] = [];
+      }
+
+      Kavie.sections[rules.section].push(target);
+
+      delete rules.section;
+    }
+
+    target.rules = rules;
+
+    function validate(newValue) {
+        var rules = target.rules;
+
+        for (key in rules) {
+            for (funcKey in Kavie.validatorFunctions) {
+                if (key == funcKey) {
+                    var isValid = Kavie.validatorFunctions[funcKey](rules[key], newValue);
+                    if (!isValid) {
+                        target.hasError(true);
+                        return;
+                    }
+                }
+            }
+        }
+
+        target.hasError(false);
+    }
+
+
+    target.startValidation = function () {
+        target.subscription = target.subscribe(validate); 
+        validate(target());
+    }
+
+    target.stopValidation = function () {
+        target.subscription.dispose();
+        target.hasError(false);
+    }
+
+    return target;
+};
