73f42cd990baafb3bad5f91153dcff758c58e899
Matthew <mnitschke@faithchapel.cc>
2016-08-22 14:02:13 -0600

    Made phone and email return false if empty

--- src/kavie.js
+++ src/kavie.js
@@ -2,7 +2,7 @@
     Kavie - knockout observable validator
     Author: Matthew Nitschke
     License: MIT (http://www.opensource.org/licenses/mit-license.php)
-    Version: 0.4.5
+    Version: 0.4.6
 */
 
 // This is a singleton pattern for the Kavie object to validate against
@@ -201,19 +201,23 @@
         return true;
     },
     phone: function (propVal, eleVal) {
-            if (eleVal.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/)) {
-                return true;
-            } else {
-                return false;
-            }
-        },
+      if (eleVal){
+        if (eleVal.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/)) {
+            return true;
+        } else {
+            return false;
+        }
+      }
+    },
     email: function (propVal, eleVal){
-            if (eleVal.match(/^(?:(?:[\w`~!#$%^&*\-=+;:{}'|,?\/]+(?:(?:\.(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?\/\.()<>\[\] @]|\\"|\\\\)*"|[\w`~!#$%^&*\-=+;:{}'|,?\/]+))*\.[\w`~!#$%^&*\-=+;:{}'|,?\/]+)?)|(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?\/\.()<>\[\] @]|\\"|\\\\)+"))@(?:[a-zA-Z\d\-]+(?:\.[a-zA-Z\d\-]+)*|\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])$/)) {
-                return true;
-            } else {
-                return false;
-            }
-        },
+      if (eleVal){
+        if (eleVal.match(/^(?:(?:[\w`~!#$%^&*\-=+;:{}'|,?\/]+(?:(?:\.(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?\/\.()<>\[\] @]|\\"|\\\\)*"|[\w`~!#$%^&*\-=+;:{}'|,?\/]+))*\.[\w`~!#$%^&*\-=+;:{}'|,?\/]+)?)|(?:"(?:\\?[\w`~!#$%^&*\-=+;:{}'|,?\/\.()<>\[\] @]|\\"|\\\\)+"))@(?:[a-zA-Z\d\-]+(?:\.[a-zA-Z\d\-]+)*|\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\])$/)) {
+            return true;
+        } else {
+            return false;
+        }
+      }
+    },
     regexPattern: function(propVal, eleVal){
       if (eleVal){
         return eleVal.toString().match(propVal) !== null;
