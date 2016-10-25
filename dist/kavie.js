05f5fa8ac2a31990cfd976a63506f38e8924fe5d
Matthew <matthewcnitschke@gmail.com>
2016-10-25 09:48:50 -0600

    Fixed incorrect version number

--- dist/kavie.js
+++ dist/kavie.js
@@ -2,7 +2,7 @@
     Kavie - knockout observable validator
     Author: Matthew Nitschke
     License: MIT (http://www.opensource.org/licenses/mit-license.php)
-    Version: 0.5.0
+    Version: 0.6.0
 */
 
 ;(function(ns){
