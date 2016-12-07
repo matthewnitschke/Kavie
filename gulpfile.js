cf70eeddc9fc094bad3555dffe2c9bd80dbafbf6
Matthew <matthewcnitschke@gmail.com>
2016-12-06 17:19:13 -0700

    Increased version number, bundled promise-polyfill with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var strip = require('gulp-strip-comments');
 
 gulp.task('scripts', function() {
-    gulp.src('src/kavie.js')
-        .pipe(rename('kavie.min.js'))
+    gulp.src(['node_modules/promise-polyfill/promise.min.js', 'src/kavie.js'])
         .pipe(uglify({ preserveComments: "license" }).on('error', gutil.log))
+        .pipe(concat('kavie.min.js'))
         .pipe(gulp.dest('dist'));
 
-    gulp.src('src/kavie.js')
+    gulp.src(['node_modules/promise-polyfill/promise.min.js', 'src/kavie.js'])
+        .pipe(concat('kavie.js'))
         .pipe(strip({ safe: true }))
         .pipe(gulp.dest('dist'));
 });
