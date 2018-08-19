e4e60c498e0460c4a344a20dfbdbb347ec3fabaa
Matthew <matthewcnitschke@gmail.com>
2018-08-18 22:05:13 -0600

    rebuilt project

--- dist/kavie.js
+++ dist/kavie.js
@@ -2,7 +2,7 @@
     Kavie - knockout observable validator
     Author: Matthew Nitschke
     License: MIT (http://www.opensource.org/licenses/mit-license.php)
-    Version: 2.4.2
+    Version: 2.5.0
 */
 
 (function (factory) {
